f8b1e2069c4bf2c9ce45c380fc3f345dfc98928d
zhengrenkun <zhengrenkun@chandashi.com>
2023-03-03 17:26:38 +0800

    feat ok

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,7 @@ import { fileURLToPath } from 'url'
 import extractZip from 'extract-zip'
 import * as cheerio from 'cheerio';
 import { zip as zipFolder } from 'zip-a-folder';
-
-import generateId from './generate-id'
 import Translator from './translator'
-import { debounce, delay } from './utils'
 
 dotenv.config()
 
@@ -21,7 +18,9 @@ async function main() {
   const book = 'EffectiveTypeScript';
   const bookPath = path.join(path.dirname(fileURLToPath(import.meta.url)), '..', 'EffectiveTypeScript.epub');
 
-  const unzipPath = path.join(__dirname, '..', book);
+  const unzipPath = path.join(__dirname, '../build', book);
+
+  // await zipFolder(unzipPath, path.join(path.dirname(bookPath), `${book}_cn.epub`));return;
 
   if (!existsSync(path.join(unzipPath, 'META-INF'))) {
     await fs.mkdir(unzipPath, { recursive: true });
@@ -43,52 +42,46 @@ async function main() {
     if (!mediaType.includes('tml')) {
       continue;
     }
+    if (!href.includes('preface01')) {
+      continue;
+    }
 
     console.log('开始翻译HTML：', href);
     const htmlPath = path.join(unzipPath, ...href.split(/\\\//));
     const html = await fs.readFile(htmlPath, { flag: 'r' });
-    const $ = cheerio.load(html);
+    const $ = cheerio.load(html.toString(), { 
+      xmlMode: true,
+      decodeEntities: false
+    });
     const pEls = $('p').toArray();
-
-    let shouldWriteId = false;
-    for (let index = 0; index < pEls.length; index++) {
-      const $el = $(pEls[index]);
-      if (!$el.attr('k-id')) {
-        $el.attr('k-id', generateId());
-        shouldWriteId = true
-      }
-    }
-    if (shouldWriteId) {
-      await fs.writeFile(htmlPath, $.html())
-    }
-
+    
     console.log(`文档总段落数：${pEls.length}`);
     for (let index = 0; index < pEls.length; index++) {
       const $el = $(pEls[index]);
-      const id = $el.attr('k-id');
       const state: string = $el.attr('k-state');
-      if (state === 'success') {
+      if (state === 'success' || state === 'never') {
         continue;
+      }
+      const msg = $el.html();
+      if (msg.split(/\s+/gi).length < parseInt(process.env.WORD_LEN)) {
+        $el.attr('k-state', 'never');
+        continue
       }
       const tryTimes = isNaN(parseInt(state)) ? 1 : parseInt(state) + 1;
       $el.attr('k-state', String(tryTimes));
-      const msg = $el.html();
       if (tryTimes > 2) {
-        console.log(`段落${id}翻译失败：`, tryTimes, msg);
+        console.log(`段落翻译失败：`, tryTimes, msg);
         continue;
       }
-      const res: any = await translator.translator(id, msg);
-      translator.getResVal(res, (v) => {
-        if (v) {
-          $el.attr('k-state', 'success');
-          $el.html(v);
-        } else {
-          console.log(`段落${id}翻译失败`, id, msg);
-        }
-      });
+      const res = await translator.sendGptMessage(`翻译\r\n${msg}`);
+      if (res?.text) {
+        $el.attr('k-state', 'success');
+        $el.html(res.text);
+      } else {
+        console.log(`段落翻译失败`, msg);
+      }
       await fs.writeFile(htmlPath, $.html(), { flag: 'w' });
     }
-    await delay(300);
     console.log('结束翻译HTML：', href);
   }
   await zipFolder(unzipPath, path.join(path.dirname(bookPath), `${book}_cn.epub`));
--- src/translator.ts
+++ src/translator.ts
@@ -1,4 +1,4 @@
-import { ChatGPTAPI } from 'chatgpt';
+import { ChatGPTAPI, ChatMessage } from 'chatgpt';
 import * as cheerio from 'cheerio';
 import nodeFetch from 'node-fetch';
 import ProxyAgent from 'simple-proxy-agent';
@@ -6,7 +6,6 @@ import { stdout as log } from 'single-line-log';
 
 export default class GptTranslator {
   private api: ChatGPTAPI;
-  private batchPrompt: { id: string, prompt: string, result?: string, resolve?: (v: string) => void }[] = [];
 
   constructor () {
     this.api = new ChatGPTAPI({
@@ -19,61 +18,15 @@ export default class GptTranslator {
     });
   }
 
-  async translator(id: string, prompt: string) {
-    const v = { id, prompt, resolve: undefined }
-    this.batchPrompt.push(v);
-    const promise = new Promise((resolve) => {
-      v.resolve = resolve;
-    })
-    const wariFlag = await this.hand();
-    if (wariFlag === true) {
-      const p = new Promise((resolve) => {
-        v.resolve = resolve;
-      })
-      return Promise.resolve(() => p)
-    } else {
-      return promise;
-    }
-  }
-
-  getResVal(res: any, cb: (v: any) => void) {
-    if (typeof res === 'function') {
-      res().then(cb)
-    } else {
-      cb(res)
-    }
-  }
-
-  async hand() {
-    const prompt = this.batchPrompt.map(v => `<p gpt-id="${v.id}">${v.prompt}</p>`).join('');
-    if (prompt.length < 300) {
-      return true;
-    }
-    const msg = `将下面的HTML文档中的英文翻译为中文并保留HTML标签\r\n${prompt}`;
-    try {
-      const res = await this.sendGptMessage(msg);
-      const $ = cheerio.load(res.text);
-      const prompts = $('p[gpt-id]').toArray();
-      for await (const v of prompts) {
-        const el = $(v);
-        const id = el.attr('gpt-id');
-        const p = this.batchPrompt.find(v => v.id === id);
-        p.result = el.html();
-      }
-    } catch (error) {
-      console.error(error)
-    }
-    this.batchPrompt.forEach(v => v.resolve(v.result));
-    this.batchPrompt = [];
-  }
-
-  private async sendGptMessage(prompt: string) {
-    console.log(prompt);
+  async sendGptMessage(prompt: string): Promise<ChatMessage> {
+    let i = 0;
+    console.log('\r\n\r\n', prompt);
     const res = await this.api.sendMessage(prompt, {
       onProgress: (partialResponse) => {
-        log(partialResponse.text)
+        log(`翻译中：${i++}%`)
       }
     });
+    console.log('\r\n', res.text);
     return res
   }
 }
